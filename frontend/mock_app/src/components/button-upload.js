c4c30224f5130139d9877e5a7d7e448f150d98f2
khushi-parikh <parikh.3@iitj.ac.in>
2022-12-14 14:09:58 +0530

    initial setup

---
+++ frontend/mock_app/src/components/button-upload.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Upload extends React.Component {
+	render() {
+		return (
+			<div>
+				uploader
+			</div>
+			
+		);
+	}
+}
+
+export default Upload;
---
+++ frontend/mock_app/src/components/selector.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "../styles/selector.css";
+
+const Selector = ({ buttons, doSomethingAfterClick }) => {
+    const [clickedId, setClickedId] = useState(-1);
+  
+    const handleClick = (event, id) => {
+      setClickedId(id);
+      doSomethingAfterClick(event);
+    };
+  
+    return (
+      <>
+        {buttons.map((buttonLabel, i) => (
+          <button
+            key={i}
+            name={buttonLabel}
+            onClick={(event) => handleClick(event, i)}
+            className={i === clickedId ? "customButton active" : "customButton"}
+          >
+            {buttonLabel}
+          </button>
+        ))}
+      </>
+    );
+};
+
+export default Selector;
