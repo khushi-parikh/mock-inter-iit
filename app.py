cc47a9bfe1b4b649366bbe2fda79dacd0b213cff
Zayed <shamshad.1@iitj.ac.in>
2022-12-14 12:56:41 +0530

    initial

---
+++ app.py
@@ -0,0 +1,44 @@
+import base64
+from io import StringIO
+import io
+from tkinter import Image
+import cv2
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from flask import Flask
+from flask_cors import CORS
+from tkinter import *
+from PIL import Image
+import numpy as np
+import imutils
+from dehazevideo import simplest_cb
+from engineio.payload import Payload
+Payload.max_decode_packets = 500
+app = Flask(__name__)
+socketio = SocketIO(app,cors_allowed_origins="*")
+CORS(app)
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('index.html')
+
+@socketio.on('image')
+def image(data_image):
+    sbuf = StringIO()
+    sbuf.write(data_image)
+    b = io.BytesIO(base64.b64decode(data_image))
+    pimg = Image.open(b)
+    frame=np.array(pimg)
+    frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+    print(frame.shape)
+    frame = imutils.resize(frame, width=500, height=375)
+    output = simplest_cb(frame, 1)
+    frame = cv2.flip(output, 1)
+    imgencode = cv2.imencode('.jpg', frame)[1]
+    stringData = base64.b64encode(imgencode).decode('utf-8')
+    b64_src = 'data:image/jpg;base64,'
+    stringData = b64_src + stringData
+    emit('response_back', stringData)
+
+if __name__ == '__main__':
+    socketio.run(app, host='127.0.0.1')
+
