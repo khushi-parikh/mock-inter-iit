3bf610daeed95e43215f06009714f6a844c4cfdb
Dhruv_Mahajan <78262624+Dhruv-Mahajan1@users.noreply.github.com>
2022-12-19 23:25:39 +0530

    Revert "Modify gitignore"

--- app.py
+++ app.py
@@ -8,33 +8,14 @@ from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 from tkinter import *
 from PIL import Image
-from werkzeug.utils import secure_filename
 import numpy as np
 import imutils
 from dehazevideo import simplest_cb
 from engineio.payload import Payload
-from flask import request
-
 Payload.max_decode_packets = 500
 app = Flask(__name__)
 socketio = SocketIO(app,cors_allowed_origins="*")
 CORS(app)
-
-file = None
-flag = False
-@app.route('/upload', methods = ['POST','GET'])
-def upload_file():
-    global file
-    global flag
-    file = request.files['file']
-    file.save(secure_filename(file.filename))
-    flag = not flag
-    # return Response(gen(file.filename), mimetype='multipart/x-mixed-replace; boundary=frame')
-    print(file.filename)
-    # video_feed()
-    return "done"
-
-
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
@@ -44,20 +25,13 @@ def savedvideo():
      return render_template('savedvideo.html')
 
 def gen():
-    video_capture = cv2.VideoCapture(secure_filename(file.filename))
-    print(secure_filename(file.filename))
-    flag2 = flag
+    video_capture = cv2.VideoCapture('testhaze.mp4')
     while True:
-        if(flag2!=flag):
-            return
-        try:
-            ret, frame = video_capture.read()
-            out = simplest_cb(frame, 1)
-            combined_frame = cv2.vconcat([frame, out])
-            frame = cv2.imencode('.jpg', combined_frame)[1].tobytes()
-            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        except:
-            return
+        ret, frame = video_capture.read()
+        out = simplest_cb(frame, 1)
+        combined_frame = cv2.hconcat([frame, out])
+        frame = cv2.imencode('.jpg', combined_frame)[1].tobytes()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/savedvideo/video_feed')
 def video_feed():
--- frontend/mock_app/src/components/button-upload.js
+++ frontend/mock_app/src/components/button-upload.js
@@ -1,8 +1,6 @@
 import { useState, useRef } from "react";
 import React from "react";
 import '../styles/button-upload.css'
-import axios from "axios";
-import Video from "./video";
 
 function Upload(){
 	const inputRef = useRef();
@@ -13,23 +11,8 @@ function Upload(){
 	const handleFileChange = (event) => {
 		const file = event.target.files[0];
 		const url = URL.createObjectURL(file);
-		var img = document.getElementById("img");
-		// img.innerHTML = "";
+		setSource(url);
 		setExt(event.target.files[0].type);
-		var formData = new FormData();
-		formData.append("file", file);
-		setSource(url);
-		// console.log(FormData);
-		axios.post("http://localhost:5000/upload", formData, {
-			headers: {	"Content-Type": "multipart/form-data"	}
-		}).then((res) => {
-			console.log(res);
-		}).catch((err) => {
-			console.log(err);
-		});
-		setSource(url);
-		// document.location.reload();
-		// img.innerHTML = "<img src = 'http://localhost:5000/savedvideo/video_feed' alt = '' class='VideoInput_image'  />";
 		// console.log(event.target.files[0]);
 		// console.log(URL.createObjectURL(event.target.files[0]));
 	};
@@ -48,7 +31,7 @@ function Upload(){
 				accept=".mov,.mp4,.jpg,.png"
 			/>
 			<button onClick={handleChoose}>Choose</button>
-			{/* {
+			{
 				source && (ext=="video/mp4" || ext=="video/quicktime") && (
 					<video
 						className="VideoInput_video"
@@ -69,11 +52,7 @@ function Upload(){
 						src={source}
 					/>
 				)
-			} */}
-			<div id="img">
-				{source && <Video></Video>}
-			</div>
-			
+			}
 			{/* <div className="VideoInput_footer">{source || "Nothing selected"}</div> */}
 		</div >
     );
