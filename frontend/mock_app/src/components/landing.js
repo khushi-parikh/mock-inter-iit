18b50d70408d5b56f4d8dc82aa67d70035e35e17
khushi-parikh <parikh.3@iitj.ac.in>
2022-12-19 19:13:59 +0530

    added hero section

---
+++ frontend/mock_app/src/components/landing.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/landing.css";
+
+const Landing = () => (
+  <div className="landing">
+    <div className="left_side">
+        <h1>Dehazing</h1>
+        <h3>One tool to see through tough hazy times!</h3>
+        <p>With just a click you can remove the dusty winds and windy blurs from an image, video or live stream and increase <b>visibility</b></p>
+    </div>
+
+    <div className="right_side">
+      <img className="image_right" src='https://media.springernature.com/lw685/springer-static/image/art%3A10.1007%2Fs00530-021-00852-z/MediaObjects/530_2021_852_Fig1_HTML.jpg' alt="header_img" />
+    </div>
+  </div>
+);
+
+export default Landing;
